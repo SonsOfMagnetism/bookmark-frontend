835105f0e550dd7f2276171adb8342594a15d435
Cindy Wong <cindywongdev@gmail.com>
2023-01-14 21:09:35 -0500

    index route progress: all routes render to screen but link doesn't work

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,20 @@
 import { useLoaderData } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Index(props) {
     // load bookmarks from api
     const bookmarks = useLoaderData()
+    console.log(bookmarks)
 
     return (<>
-      <div className="bookmarks">
-          <h1>Index Page</h1>
+      <div className="bookmarks-container">
+          {bookmarks.map(bookmark => (
+            <div className="bookmark-card" key={bookmark._id}>
+              <Link to={bookmark.url}>
+                <div className="bookmark-text">{bookmark.title}</div>
+              </Link>
+            </div>
+          ))}
       </div>
     </>)
   }
