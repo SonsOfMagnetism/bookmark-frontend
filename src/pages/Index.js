9f5ceeccf12e03686a03313c6f2e25921d00d848
Alexei Rusu <rusualexei1995@gmail.com>
2023-01-16 22:09:44 -0500

    serchbox implemented

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,27 +1,47 @@
 import { useLoaderData } from 'react-router-dom'
 import { Form } from "react-router-dom"
+import { useState } from "react"
 
 function Index(props) {
 
   // load bookmarks from api
   const bookmarks = useLoaderData()
   console.log(bookmarks)
+
+
+  const [value, setValue] = useState("")
   
+  const onChange = (event) => {
+      setValue(event.target.value)
+  }
+
+
   return (<>
     <div>
       <h2>Create a Bookmark</h2>
       <Form action="/create" method="post">
         <input type="input" name="title" placeholder="bookmark title" />
         <input type="input" name="url" placeholder="add url" />
-        
-
         <input type="submit" value="create bookmark" /> 
       </Form>
+      
+    </div>
+     
+    <div className="bookmarks-container">
+
+    <div>
+        <input type="text" placeholder="Search..." value={value} onChange={onChange}/>
     </div>
 
-    <div className="bookmarks-container">
-        {bookmarks.map(bookmark => (
-          <div className="bookmark-card" key={bookmark._id}>
+      {bookmarks.filter(item => {
+
+        const searchWord = value.toLowerCase()
+        const title = item.title.toLowerCase()
+
+        return title.startsWith(searchWord)
+      })
+        .map(bookmark => (
+          <div  className="bookmark-card" key={bookmark._id}>
             <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
               <div className="bookmark-text">{bookmark.title}</div>
             </a>
